eb82eef751e576b87de334c9ef7cfb9d1e843cb4
Danillo Castro <56567521+danillotc@users.noreply.github.com>
2020-05-18 14:18:19 -0300

    Add files via upload

--- controls.js
+++ controls.js
@@ -1,10 +1,18 @@
 function toggledropdown(nome) {
+    
+    var dropdowns = document.getElementsByClassName("dropdown");
     var elem = document.getElementById(nome);
+    var icon = $("i",$(elem).parent());
     var estilo = getComputedStyle(elem);
     
     if (estilo.display == "none" || elem.style.display == "none") {
-        elem.style.display = "block";
+        for (var i = 0; i < dropdowns.length; i++) {
+            dropdowns.item(i).style.display = "none";
+        }
+        $(elem).toggle();
+        $(icon).toggleClass("fas fa-angle-right fas fa-angle-down");
     } else {
-        elem.style.display = "none";
+        $(icon).toggleClass("fas fa-angle-right fas fa-angle-down");
+        $(elem).toggle();
     }
 }
